735ff198c29f4c30556cb7e7f4ea9271cd22cb45
DESKTOP-P9KEFNF\zackb <zackbearss@gmail.com>
2017-02-03 20:47:01 -0500

    All of SList with tail pointer coded
    - trying to implement the test code
    - fix all errors

---
+++ SList with Tail/src/SList.java
@@ -0,0 +1,351 @@
+public class SList<T> {
+	SNode<T> head;
+	SNode<T> tail;
+	int size;
+	
+	/**
+	 * Constructor for SList
+	 */
+	SList()
+	{
+		//init vars
+		size = 0;
+		head = new SNode<>(null, null);
+	}
+	
+	/**
+	 * Returns the size of the list
+	 * @return size
+	 */
+	int size()
+	{
+		return size;
+	}
+	
+	/**
+	 * Checks if the list is empty
+	 * @return True if empty / False if not empty
+	 */
+	boolean empty()
+	{
+		return (size == 0);
+	}
+
+	/**
+	 * Returns the value at a certain index
+	 * @param index The index in the list
+	 * @return Value at the index
+	 */
+	T value_at(int index)
+	{
+		//check size
+		if(index >= size)
+		{
+			return null;
+		}
+		//fast access to head
+		if(index == 0)
+		{
+			return head.value;
+		}
+		//fast access to tail
+		if(index == size - 1)
+		{
+			return tail.value;
+		}
+		//look through rest of list
+		int counter = 1;
+		SNode<T> temp = head.next;
+		//traverse list
+		while(counter != index)
+		{
+			temp = temp.next;
+			counter++;
+		}
+		return temp.value;
+	}
+
+	/**
+	 * Push a new node at the front of the list
+	 * @param value The value to go in the new node
+	 */
+	void push_front(T value)
+	{
+		//check size, if 0, update heaad
+		if(size == 0)
+		{
+			head.value = value;
+			size++;
+			return;
+		}
+		//change head with new node
+		SNode<T> temp = new SNode<>(value, head);
+		head = temp;
+		size++;
+	}
+	
+	/**
+	 * Removes the first node of the list and returns
+	 * @return Value that was popped
+	 */
+	T pop_front()
+	{
+		//check size
+		if(size == 0)
+		{
+			return null;
+		}
+		//pop head
+		T value = head.value;
+		head = head.next;
+		size--;
+		return value;
+	}
+	
+	/**
+	 * Create a new node at the end of the list
+	 * @param value The value that goes into the new node
+	 */
+	void push_back(T value)
+	{
+		//check size
+		if(size == 0)
+		{
+			head.value = value;
+			size++;
+			return;
+		}
+		SNode<T> temp = new SNode<T>(value, null);
+		tail.next = temp;
+		tail = temp;
+		size++;
+	}
+	
+	/**
+	 * Remove the last node of the list and return the value
+	 * @return The value that was popped
+	 */
+	T pop_back()
+	{
+		//check size
+		if(size == 0)
+		{
+			return null;
+		}
+		SNode<T> traverse = head;
+		//traverse list
+		while(traverse.next != tail)
+		{	
+			traverse = traverse.next;
+		}
+		
+		T value = tail.value;
+		traverse.next = null;
+		tail = traverse;
+		size--;
+		return value;
+	}
+	
+	/**
+	 * Returns the value of the first node
+	 * @return The value in the first node
+	 */
+	T front()
+	{
+		//check size
+		if(size == 0)
+		{
+			return null;
+		}
+		return head.value;
+	}
+	
+	/**
+	 * Returns the value in the last node
+	 * @return The value in the last node
+	 */
+	T back()
+	{
+		//check size
+		if(size == 0)
+		{
+			return null;
+		}	
+		return tail.value;
+	}
+	
+	/**
+	 * Inserts a new node at a certain index of the list
+	 * @param index The index to place the node
+	 * @param value The value that goes in the new node
+	 */
+	void insert(int index, T value)
+	{
+		//check size, if the size is 0 and index is 0, go ahead
+		if(size == 0)
+		{
+			if(index == 0)
+			{
+				this.push_back(value);
+			}
+			return;
+		}
+		//fast access to head
+		if(index == 0)
+		{
+			this.push_front(value);
+		}
+		//fast access to tail
+		if(index == size - 1)
+		{
+			this.push_back(value);
+		}
+		
+		SNode<T> temp = new SNode<>(value, null);
+		SNode<T> traverseCurrent = head;
+		SNode<T> traversePast = head;
+		int counter = 0;
+		
+		//traverse list
+		while(counter != index)
+		{
+			if(traverseCurrent == traversePast)
+			{
+				traverseCurrent = traverseCurrent.next;
+			}
+			else
+			{
+				traversePast = traverseCurrent;
+				traverseCurrent = traverseCurrent.next;
+			}
+			counter++;
+		}
+		
+		traversePast.next = temp;
+		temp.next = traverseCurrent;
+		size++;
+	}
+	
+	/**
+	 * Erases a node at a certain index
+	 * @param index The index of the node to erase
+	 */
+	void erase(int index)
+	{
+		//check size
+		if(size == 0)
+		{
+			return;
+		}
+		//fast head access
+		if(index == 0)
+		{
+			this.pop_front();
+			return;
+		}
+		//fast tail access
+		if(index == size - 1)
+		{
+			this.pop_back();
+			return;
+		}
+		
+		SNode<T> traverseCurrent = head;
+		SNode<T> traversePast = head;
+		int counter = 0;
+		//traverse list
+		while(counter != index)
+		{
+			if(traverseCurrent == traversePast)
+			{
+				traverseCurrent = traverseCurrent.next;
+			}
+			else
+			{
+				traversePast = traverseCurrent;
+				traverseCurrent = traverseCurrent.next;
+			}
+			
+			counter++;
+		}
+		
+		traversePast.next = traverseCurrent.next;
+		size--;
+	}
+	
+	/**
+	 * Returns the value of the node n from the end
+	 * @param n How many nodes away from the end
+	 * @return Value from the selected node
+	 */
+	T value_n_from_end(int n)
+	{
+		//check size, and make sure n from end makes sense
+		if(size == 0 || (size - n) <= 0)
+		{
+			return null;
+		}
+		
+		SNode<T> traverse = head;
+		int counter = 0;
+		//traverse list
+		while(counter != size - n - 1)
+		{
+			traverse = traverse.next;
+			counter++;
+		}
+		return traverse.value;
+	}
+	
+	/**
+	 * Reverse a linked list
+	 */
+	void reverse()
+	{
+		//check size, only makes sense that size is 2 or more
+		if(size < 2)
+		{
+			return;
+		}
+		
+		SNode<T> reverseHead  = tail;
+		SNode<T> reverseTraverse = reverseHead;
+		SNode<T> reversalTempStorage = new SNode<>(null, null);
+		
+		for(int i = size - 2; i >= 0; i--)
+		{
+			reversalTempStorage.value = this.value_at(i);
+			reversalTempStorage.next = null;
+			
+			reverseTraverse.next = reversalTempStorage;
+			reverseTraverse = reverseTraverse.next;
+			reversalTempStorage = new SNode<>(null, null);
+		}
+		tail = head;
+		head = reverseHead;
+	}
+	
+	/**
+	 * Remove the first node to contain a certain value
+	 * @param value The value to look for
+	 */
+	void remove_value(T value)
+	{
+		//check size
+		if(size == 0)
+		{
+			return;
+		}
+		
+		SNode<T> traverse = head;
+		for(int i = 0; i < size; i++)
+		{
+			if(traverse.value == value)
+			{
+				this.erase(i);
+				return;
+			}
+			traverse = traverse.next;
+		}
+	}
+}
---
+++ SList with Tail/src/SListTesting.java
@@ -0,0 +1,206 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SListTesting {
+
+	@Test
+	public void testSize() {
+		SList<Integer> ll = new SList<Integer>();
+		ll.push_back(10);
+		ll.push_back(4);
+		ll.push_back(6);
+		assertEquals(3, ll.size());
+		ll.pop_back();
+		assertEquals(2, ll.size());
+		ll.push_front(3);
+		ll.push_front(5);
+		assertEquals(4, ll.size());
+		ll.insert(2, 99);
+		assertEquals(5, ll.size());
+		ll.erase(4);
+		assertEquals(4, ll.size());
+		ll.pop_front();
+		assertEquals(3, ll.size());
+		ll.remove_value(99);
+		assertEquals(2, ll.size());
+	}
+	
+	@Test
+	public void testEmpty() {
+		SList<Integer> ll = new SList<Integer>();
+		assertEquals(true, ll.empty());
+		ll.push_back(10);
+		ll.push_back(4);
+		ll.push_back(6);
+		assertEquals(false, ll.empty());
+		ll.pop_back();
+		ll.pop_back();
+		ll.pop_back();
+		assertEquals(true, ll.empty());
+	}
+	
+	@Test
+	public void testValue_at() {
+		SList<Integer> ll = new SList<Integer>();
+		ll.push_back(10);
+		ll.push_back(4);
+		ll.push_back(6);
+		assertEquals(4, (int)ll.value_at(1));
+		assertEquals(6, (int)ll.value_at(2));
+		assertNull(ll.value_at(3));
+		ll.pop_back();
+		ll.pop_back();
+		assertEquals(10, (int)ll.value_at(0));
+		ll.pop_back();
+		assertNull(ll.value_at(0));
+	}
+	
+	@Test
+	public void testPush_front() {
+		SList<Integer> ll = new SList<Integer>();
+		ll.push_front(10);
+		ll.push_front(4);
+		ll.push_front(6);
+		assertEquals(10, (int)ll.value_at(2));
+		assertEquals(4, (int)ll.value_at(1));
+		assertEquals(6, (int)ll.value_at(0));
+	}
+	
+	@Test
+	public void testPop_front() {
+		SList<Integer> ll = new SList<Integer>();
+		ll.push_back(10);
+		ll.push_back(4);
+		ll.push_back(6);
+		assertEquals(10, (int)ll.pop_front());
+		assertEquals(4, (int)ll.pop_front());
+		assertEquals(6, (int)ll.pop_front());
+		assertNull(ll.pop_front());
+	}
+		
+	@Test
+	public void testPush_back() {
+		SList<Integer> ll = new SList<Integer>();
+		ll.push_back(10);
+		ll.push_back(4);
+		ll.push_back(6);
+		assertEquals(10, (int)ll.value_at(0));
+		assertEquals(4, (int)ll.value_at(1));
+		assertEquals(6, (int)ll.value_at(2));
+	}
+	
+	@Test
+	public void testPop_back() {
+		SList<Integer> ll = new SList<Integer>();
+		ll.push_back(10);
+		ll.push_back(4);
+		ll.push_back(6);
+		assertEquals(6, (int)ll.pop_back());
+		assertEquals(4, (int)ll.pop_back());
+		assertEquals(10, (int)ll.pop_back());
+		assertNull(ll.pop_back());
+	}
+	
+	@Test
+	public void testFront() {
+		SList<Integer> ll = new SList<Integer>();
+		assertNull(ll.front());
+		ll.push_back(10);
+		ll.push_back(4);
+		ll.push_back(6);
+		assertEquals(10, (int)ll.front());
+	}
+	
+	@Test
+	public void testBack() {
+		SList<Integer> ll = new SList<Integer>();
+		assertNull(ll.back());
+		ll.push_back(10);
+		ll.push_back(4);
+		ll.push_back(6);
+		assertEquals(6, (int)ll.back());
+	}
+	
+	@Test
+	public void testInsert() {
+		SList<Integer> ll = new SList<Integer>();
+		ll.insert(10, 5);
+		ll.insert(0, 10);
+		ll.push_back(4);
+		ll.push_back(6);
+		ll.insert(2, 99);
+		assertEquals(10, (int)ll.value_at(0));
+		assertEquals(4, (int)ll.value_at(1));
+		assertEquals(99, (int)ll.value_at(2));
+		assertEquals(6, (int)ll.value_at(3));
+	}
+	
+	@Test
+	public void testErase() {
+		SList<Integer> ll = new SList<Integer>();
+		ll.erase(0);
+		ll.insert(0, 10);
+		ll.push_back(4);
+		ll.push_back(6);
+		ll.insert(2, 99);
+		ll.erase(0);
+		assertEquals(4, (int)ll.value_at(0));
+		assertEquals(99, (int)ll.value_at(1));
+		assertEquals(6, (int)ll.value_at(2));
+		assertNull(ll.value_at(3));
+		ll.erase(1);
+		assertEquals(4, (int)ll.value_at(0));
+		assertEquals(6, (int)ll.value_at(1));
+	}
+	
+	@Test
+	public void testValue_n_from_end() {
+		SList<Integer> ll = new SList<Integer>();
+		ll.push_back(10);
+		ll.push_back(4);
+		ll.push_back(6);
+		ll.push_back(2);
+		ll.push_back(56);
+		ll.push_back(234);
+		assertEquals(4, (int)ll.value_n_from_end(4));
+		assertNull(ll.value_n_from_end(6));
+	}
+	
+	@Test
+	public void testReverse() {
+		SList<Integer> ll = new SList<Integer>();
+		ll.push_back(10);
+		ll.push_back(4);
+		ll.push_back(6);
+		ll.push_back(2);
+		ll.push_back(56);
+		ll.push_back(234);
+		ll.reverse();
+		assertEquals(234, (int)ll.value_at(0));
+		assertEquals(56, (int)ll.value_at(1));
+		assertEquals(2, (int)ll.value_at(2));
+		assertEquals(6, (int)ll.value_at(3));
+		assertEquals(4, (int)ll.value_at(4));
+		assertEquals(10, (int)ll.value_at(5));
+	}
+	
+	@Test
+	public void testRemove_value() {
+		SList<Integer> ll = new SList<Integer>();
+		ll.remove_value(10);
+		ll.push_back(10);
+		ll.push_back(4);
+		ll.push_back(10);
+		ll.push_back(2);
+		ll.push_back(56);
+		ll.push_back(234);
+		ll.remove_value(10);
+		assertEquals(4, (int)ll.value_at(0));
+		assertEquals(10, (int)ll.value_at(1));
+		assertEquals(2, (int)ll.value_at(2));
+		assertEquals(56, (int)ll.value_at(3));
+		assertEquals(234, (int)ll.value_at(4));
+	}
+
+}
