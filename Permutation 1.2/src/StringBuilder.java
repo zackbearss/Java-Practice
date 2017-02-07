45d9d15881175906b875021ed0fcf516a45edf7f
Zack Bearss <zbearss@riversidemfg.com>
2017-02-07 15:36:18 -0500

    Added comment to permutation function of other methods
    - turns out I did it pretty well, added comment showing other ways of doing the same thing.. even one to improve mine to reduce space

--- Permutation 1.2/src/StringBuilder.java
+++ Permutation 1.2/src/StringBuilder.java
@@ -33,6 +33,9 @@ public class StringBuilder {
 	{
 		//O(3N) -> O(N) run time
 		//O(3N) -> O(N) space
+		//could sort it first and compare strings, that is O(N log N)
+		//better way of doing it this way is to have one hash, add to it with textHash, subtract
+		//from it with permutation, make sure it is all 0
 		Hashtable<Character, Integer> textHash = CharacterMap(this.text);
 		Hashtable<Character, Integer> permutationHash = CharacterMap(permutation);
 		
