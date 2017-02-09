0cb0bb9288f875578f4fcc713be890f4035953aa
Zack Bearss <zbearss@riversidemfg.com>
2017-02-09 10:11:14 -0500

    Start of queue with array

---
+++ Queue Array/src/QueueArray.java
@@ -0,0 +1,20 @@
+
+public class QueueArray<T> {
+	private Object[] queue;
+	private Object head, tail;
+	private int size;
+	
+	public QueueArray(int size)
+	{
+		this.size = size;
+		queue = new Object[size];
+		queue[0] = head = tail;
+	}
+	
+	public enqueue(T value)
+	{
+		if()
+	}
+	
+	
+}
