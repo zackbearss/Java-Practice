dcbda6afe864765dea0540e7607fb619e920fa7e
Zack Bearss <zbearss@riversidemfg.com>
2017-02-09 13:32:52 -0500

    Finished adding testing code

--- Queue Array/src/QueueArrayTesting.java
+++ Queue Array/src/QueueArrayTesting.java
@@ -6,7 +6,7 @@ public class QueueArrayTesting {
 
 	@Test
 	public void testEnqueue() {
-		QueueArray<Integer> qa = new QueueArray<>(5);
+		QueueArray<Integer> qa = new QueueArray<>(6);
 		assertEquals(true, qa.empty());
 		qa.enqueue(10);
 		assertEquals(false, qa.empty());
@@ -19,7 +19,7 @@ public class QueueArrayTesting {
 
 	@Test
 	public void testDequeue() {
-		QueueArray<Integer> qa = new QueueArray<>(5);
+		QueueArray<Integer> qa = new QueueArray<>(6);
 		assertEquals(true, qa.empty());
 		qa.enqueue(10);
 		qa.enqueue(10);
@@ -38,12 +38,30 @@ public class QueueArrayTesting {
 
 	@Test
 	public void testEmpty() {
-		fail("Not yet implemented");
+		QueueArray<Integer> qa = new QueueArray<>(6);
+		assertEquals(true, qa.empty());
+		qa.enqueue(10);
+		assertEquals(false, qa.empty());
+		assertEquals(10, (int)qa.dequeue());
+		assertEquals(true, qa.empty());
 	}
 
 	@Test
 	public void testFull() {
-		fail("Not yet implemented");
+		QueueArray<Integer> qa = new QueueArray<>(6);
+		assertEquals(false, qa.full());
+		qa.enqueue(10);
+		qa.enqueue(10);
+		qa.enqueue(1);
+		qa.enqueue(0);
+		qa.enqueue(5);
+		assertEquals(true, qa.full());
+		assertEquals(10, (int)qa.dequeue());
+		assertEquals(10, (int)qa.dequeue());
+		assertEquals(1, (int)qa.dequeue());
+		assertEquals(0, (int)qa.dequeue());
+		assertEquals(5, (int)qa.dequeue());
+		assertEquals(false, qa.full());
 	}
 
 }
